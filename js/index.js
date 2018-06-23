2dc3dc010d11c2193127de6a1f121b0428b2a0b8
Your Name <you@example.com>
2018-06-23 18:06:55 +0530

    dots

--- js/index.js
+++ js/index.js
@@ -3,13 +3,25 @@ new Vue({
     data: {
         url: ['img/elyse.jpeg','img/matthew.jpg','img/molly.jpg'],
         currentNumber: 0,
+        timer: null,
+        index: ['0','1','2']
     },
 
     mounted: function () {
+        this.dotrotation();
         this.startRotation();
     },
 
     methods: {
+
+        dotrotation: function() {
+            var dots = document.getElementsByClassName("dot");
+            for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+            }
+            dots[this.currentNumber].className += " active";
+        },
+
         startRotation: function() {
             this.timer = setInterval(this.next, 3000);
         },
@@ -21,9 +33,51 @@ new Vue({
 
         next: function() {
             this.currentNumber += 1
+            if(this.currentNumber==3)
+                this.currentNumber=0;
+            var dots = document.getElementsByClassName("dot");
+            for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+            }
+            dots[this.currentNumber].className += " active";
         },
+
         prev: function() {
             this.currentNumber -= 1
+            if(this.currentNumber<0)
+                this.currentNumber=2;
+            var dots = document.getElementsByClassName("dot");
+            for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+            }
+            dots[this.currentNumber].className += " active";
+        },
+
+        dot1: function() {
+            this.currentNumber=0
+            var dots = document.getElementsByClassName("dot");
+            for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+            }
+            dots[this.currentNumber].className += " active";
+        },
+
+        dot2: function() {
+            this.currentNumber=1
+            var dots = document.getElementsByClassName("dot");
+            for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+            }
+            dots[this.currentNumber].className += " active";
+        },
+
+        dot3: function() {
+            this.currentNumber=2
+            var dots = document.getElementsByClassName("dot");
+            for (i = 0; i < dots.length; i++) {
+            dots[i].className = dots[i].className.replace(" active", "");
+            }
+            dots[this.currentNumber].className += " active";
         }
     },
 });
